1aea90cbe162f39ee5723bf74bccfd69ecedda9f
Karan Trehan <karan.trehan@springworks.in>
2024-10-21 04:11:16 +0530

    Initial Sockets and DPI setup

---
+++ src/utils/DPI.js
@@ -0,0 +1,40 @@
+class DPI {
+  constructor() {
+    this.injector = {};
+  }
+
+  /*
+   * @desciption: Inject a function to DPI
+   * @param {String} name: Name of the module
+   * @param {Object} resolverFn: resolverFn to be injected
+   * @return {Void}
+   */
+  factory(name, resolverFn) {
+    this.injector[name] = resolverFn();
+  }
+
+  /*
+   * @desciption: Inject a module/ table to DPI
+   * @param {String} name: Name of the module
+   * @param {Object} module: Module to be injected
+   * @return {Void}
+   */
+  module(name, module) {
+    this.injector[name] = module;
+  }
+
+  /*
+   * @desciption: Get the module/function from DPI
+   * @param {String} name: Name of the module
+   * @return {Object} module/function
+   * @throws {Error} if module not found
+   */
+  get(name) {
+    if (this.injector[name]) {
+      return this.injector[name];
+    }
+    throw Error(`DPI: ${name} not found`);
+  }
+}
+
+module.exports = new DPI();
---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const DPI = require("./DPI");
+
+const list = ["Sockets"];
+
+try {
+  let Util;
+  list.forEach((name) => {
+    Util = require(`./${name}`);
+    DPI.factory(name, () => new Util());
+  });
+} catch (error) {
+  console.error(error);
+}
